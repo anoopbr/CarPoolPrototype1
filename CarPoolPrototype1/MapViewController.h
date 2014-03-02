40f523c39f8e83db1fb76fead760d5082a1f192f
anoopbr <anoop.br@gmail.com>
2014-03-02 17:05:33 -0500

    Added Navigation for Browse Trips

--- CarPoolPrototype1/MapViewController.h
+++ CarPoolPrototype1/MapViewController.h
@@ -1,13 +1,13 @@
 //
-//  ViewController.h
+//  MapViewController.h
 //  CarPoolPrototype1
 //
-//  Created by Anoop Balakrishnan Rema on 3/1/14.
+//  Created by Anoop Balakrishnan Rema on 3/2/14.
 //  Copyright (c) 2014 Anoop Balakrishnan Rema. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface MapViewController : UIViewController
 
 @end
